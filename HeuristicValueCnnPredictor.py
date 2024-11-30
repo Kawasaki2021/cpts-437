0a5a20ecbaa8edfe9fa407c92b296b96fba00870
Johnjacobjames Erdman <j3erdman@gmail.com>
2024-11-30 12:47:41 -0800

    Update HeuristicValueCnnPredictor.py to remove unused libraries

--- HeuristicValueCnnPredictor.py
+++ HeuristicValueCnnPredictor.py
@@ -1,8 +1,6 @@
 import numpy as np
-import tensorflow as tf
 from tensorflow.keras.models import Model
-from tensorflow.keras.layers import Input, Conv2D, MaxPooling2D, Flatten, Dense, Concatenate
-from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.layers import Input, Conv2D, MaxPooling2D, Flatten, Dense
 
 ##### Create CNN model for face recognition #####
 def Create_CNN_Model(input_shape):
