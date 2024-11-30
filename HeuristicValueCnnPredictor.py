7f4a0efcb9ad134e6212c4a03ce0d2e446081da2
Johnjacobjames Erdman <j3erdman@gmail.com>
2024-11-30 12:06:51 -0800

    Update HeuristicValueCnnPredictor.py

--- HeuristicValueCnnPredictor.py
+++ HeuristicValueCnnPredictor.py
@@ -1,31 +1,5 @@
-### Building CNN Model ###
-
+import numpy as np
 import tensorflow as tf
-from tensorflow.keras import layers, models
-
-# Define the CNN model
-def create_cnn(input_shape=(224, 224, 3)):
-    model = models.Sequential()
-    model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=input_shape))
-    model.add(layers.MaxPooling2D((2, 2)))
-    
-    model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-    model.add(layers.MaxPooling2D((2, 2)))
-    
-    model.add(layers.Conv2D(128, (3, 3), activation='relu'))
-    model.add(layers.MaxPooling2D((2, 2)))
-    
-    model.add(layers.Flatten())
-    model.add(layers.Dense(128, activation='relu'))
-    # 3 output classes: Kamala Harris, Donald Trump, Unknown
-    model.add(layers.Dense(3, activation='softmax'))
-    
-    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-    return model
-
-# Create the model
-face_recognition_model = create_cnn()
-
-### Preprocess Frames for Model ###
-
-### Predict Faces ###
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input, Conv2D, MaxPooling2D, Flatten, Dense, Concatenate
+from tensorflow.keras.utils import to_categorical
