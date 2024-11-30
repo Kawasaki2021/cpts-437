1c6ed11abd0149caf2ef4d1004b677948912f90d
Johnjacobjames Erdman <j3erdman@gmail.com>
2024-11-30 12:29:05 -0800

    Update HeuristicValueCnnPredictor.py to change Create_CNN_Model() to have multiple convolutional layers

--- HeuristicValueCnnPredictor.py
+++ HeuristicValueCnnPredictor.py
@@ -5,25 +5,28 @@ from tensorflow.keras.layers import Input, Conv2D, MaxPooling2D, Flatten, Dense,
 from tensorflow.keras.utils import to_categorical
 
 ##### Create CNN model for face recognition #####
-def create_cnn_model(input_shape):
-    # Input layer for the face image with the given input shape
+def Create_CNN_Model(input_shape):
     face_input = Input(shape=input_shape)
 
     # First convolutional layer
     x = Conv2D(32, (3, 3), activation='relu')(face_input)
-
-    # Max-pooling layer
     x = MaxPooling2D(pool_size=(2, 2))(x)
 
-    # Flatten the output of the pooling layer
+    # Second convolutional layer
+    x = Conv2D(64, (3, 3), activation='relu')(x)
+    x = MaxPooling2D(pool_size=(2, 2))(x)
+
+    # Third convolutional layer
+    x = Conv2D(128, (3, 3), activation='relu')(x)
+    x = MaxPooling2D(pool_size=(2, 2))(x)
+
+    # Flatten and fully connected layers
     x = Flatten()(x)
-
-    # Fully connected layer
     x = Dense(128, activation='relu')(x)
-
-    # Output layer
-    # Output: Kamala, Trump, Mediator, Unknown
     face_output = Dense(4, activation='softmax')(x)
 
-    # Create and return model with input layer and output layer
-    return Model(inputs=face_input, outputs=face_output)
+    # Create and compile the model
+    model = Model(inputs=face_input, outputs=face_output)
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+    return model
